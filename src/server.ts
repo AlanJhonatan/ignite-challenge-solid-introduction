635eda74eac99fa37c58057a7e45052b5a317098
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-10 22:18:30 -0300

    docs: created an basic swagger documentation

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
 import { app } from ".";
 
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3333, () => {
+  console.log("🔥 Server is running at: http://localhost:3333");
+  console.log("📄 API Docs is running at: http://localhost:3333/api-docs");
+});
